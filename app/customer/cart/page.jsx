b6b69d68346c8ea61191654c11485720711ed0e6
microsoftwala <124462509+microsoftwala@users.noreply.github.com>
2024-03-22 09:31:30 +0530

    Update page.jsx

--- app/customer/cart/page.jsx
+++ app/customer/cart/page.jsx
@@ -1,56 +1,139 @@
-"use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../../../styles/cart.css";
 import { useRouter } from "next/navigation";
-import Card3 from "../../../components/Card3";
-import {loadStripe} from '@stripe/stripe-js';
+import Card3 from "../../../components/Cardplace";
+import Oops from "@components/Oops";
+import Lottie from "lottie-react";
+import animationData from "../../../components/Animation - 1710692268578.json";
 
-const page = ({ confirmOrder, setConfirms, items }) => {
+const Page = ({
+  confirmOrder,
+  setConfirms,
+  submitOrder,
+  items,
+  Counterminus,
+  Counterplus,
+  count,
+  menuitem,
+  buy,
+}) => {
   const router = useRouter();
   const goBack = () => {
-    console.log("Hi");
     setConfirms(false);
   };
+  const [foodid, setFoodid] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [toggle, setToggle] = useState(false);
 
-  const [amount,setamount]=useState(100);
+  useEffect(() => {
+    const foodIds = [];
+    for (let key in count) {
+      if (!isNaN(key) && count[key] > 0) {
+        foodIds.push(parseInt(key));
+      }
+    }
+    setFoodid(foodIds);
+  }, [count]);
 
+  useEffect(()=>{
+    submitOrder();
+  },[count])
 
- 
-  const handlePayment=()=>{
-    console.log("done");
-  }
+  useEffect(() => {
+    let newTotal = 0;
+    menuitem.forEach((item, index) => {
+      if (foodid.includes(index)) {
+        newTotal += item.itemprice * count[index];
+      }
+    });
+    setTotal(newTotal);
+  }, [count, foodid]);
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
 
   return (
-    <div className="parent mt-10 w-full">
-      <div id="header">
-        <div className="flex justify-start ml-20 w-full">
-          <button className="text-2xl font-bold button1" onClick={goBack}>
-            ← Back
-          </button>
+    <div className="w-full">
+      {toggle && <div className="overlay" />}
+      {toggle && (
+        <div>
+          <Lottie
+            animationData={animationData}
+            className="lottie-animation"
+            onClick={() => {
+              buy();
+            }}
+          />
         </div>
-        <div className="heading text-3xl">Your Order</div>
-        <div className="subheading">
-          order a delicious meal from our rest-around menu
-        </div>
-        <div className="flex w-5/6 overflow-y-hidden">
-          <div className="flex card-container justify-center">
-            {/* {/* {items.map((item, index) => { */}
-              {/* <div> */}
-                {/* {" "}  */}
-                <Card3 />
-                <Card3 />
-                <Card3 />
-                <Card3 />
-                {/* {item.customerId} */}
-              {/* </div>; */}
-            {/* })} */}
+      )}
+      {!toggle && (
+        <div
+          className="m-4 bg-gradient-to-l from-gray-400 to-slate-500 rounded-2xl p-6"
+          style={{ background: "linear-gradient(to right, #ff9a9e, #fad0c4)" }}
+        >
+          <div className="flex justify-center bg-gradient-to-b from-gray-600 to-indigo-400 rounded-lg p-4 md:text-6xl text-4xl font-dance font-bold text-white shadow-2xl mb-6">
+            Order Summary
+          </div>
+          <div>
+            <button onClick={() => goBack()} className="btn">
+              ←Back
+            </button>
+          </div>
+          <div>
+            {foodid.length !== 0 ? (
+              <div>
+                {menuitem.map((item, index) => (
+                  <div className="flex w-full" key={index}>
+                    {foodid.map((key, index1) => {
+                      if (key === index && count[index] !== 0) {
+                        return (
+                          <div className="flex w-full" key={index1}>
+                            <Card3
+                              itemname={item.itemname}
+                              quantity={count[index]}
+                              price={item.itemprice}
+                              Counterminus={Counterminus}
+                              Counterplus={Counterplus}
+                              index2={index}
+                              count={count}
+                              url={item.imageurl}
+                            />
+                          </div>
+                        );
+                      } else {
+                        return null;
+                      }
+                    })}
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <div>
+                <Oops goBack={goBack} />
+              </div>
+            )}
+          </div>
+          <div className="flex justify-center">
+            <p className="bg-slate-100 mt-6 p-3 w-11/12 flex justify-center text-2xl font-dance font-bold rounded-2xl">
+              Total Price: {total}
+            </p>
+          </div>
+          <div className="flex justify-center mt-8 mb-4">
+            <button
+              className="button"
+              onClick={() => {
+                handleToggle();
+                // confirmOrder();
+              }}
+            >
+              Placed Order→
+            </button>
           </div>
         </div>
-
-        <button className="button" onClick={handlePayment}>—Placed Order—</button>
-      </div>
+      )}
     </div>
   );
 };
 
-export default page;
+export default Page;
